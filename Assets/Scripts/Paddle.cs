690391fd7fdb22c86761592d95803ef9a0723906
Germán Lobbia <germanlobbia@gmail.com>
2023-12-05 21:31:18 -0300

    Paddle Movement Added

--- Assets/Scripts/Paddle.cs
+++ Assets/Scripts/Paddle.cs
@@ -1,18 +1,34 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
+
+[RequireComponent(typeof(Rigidbody2D)), RequireComponent(typeof(BoxCollider2D))]
 public class Paddle : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    Rigidbody2D _rigidBody2D;
+    Rigidbody2D rigidBody2D => _rigidBody2D = _rigidBody2D ? _rigidBody2D : GetComponent<Rigidbody2D>();
+
+    float speed = 30f;
+    Vector2 direction;
+
+    private void Update()
     {
-        
+        if (Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.LeftArrow))
+        {
+            direction = Vector2.left;
+        }
+        else if (Input.GetKey(KeyCode.D) || Input.GetKey(KeyCode.RightArrow))
+        {
+            direction = Vector2.right;
+        }
+        else
+        {
+            direction = Vector2.zero;
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+    private void FixedUpdate()
     {
-        
+        if (direction != Vector2.zero)
+            rigidBody2D.AddForce(direction * speed);
     }
 }
